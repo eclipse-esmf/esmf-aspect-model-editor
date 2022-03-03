13c34b1165d4a36915116bd7410718be7d3c2e1f
Michele Santoro <michele.santoro@de.bosch.com>
2022-03-03 10:52:07 +0100

    Upgrade ame version to 3.0.0

--- apps/bame/src/app/components/help/document.component.ts
+++ apps/bame/src/app/components/help/document.component.ts
@@ -7,7 +7,7 @@ import {MatDialogRef} from '@angular/material/dialog';
   styleUrls: ['./document.component.scss'],
 })
 export class DocumentComponent {
-  BAMEDocumentationLink = 'http://docs.digital-twin.bosch-nexeed.com/bame-guide/2.9.2/introduction.html'; // NOSONAR
+  BAMEDocumentationLink = 'http://docs.digital-twin.bosch-nexeed.com/ame-guide/3.0.0/introduction.html'; // NOSONAR
   BAMMDocumentationLink = 'https://openmanufacturingplatform.github.io/sds-bamm-aspect-meta-model/bamm-specification/v1.0.0/index.html';
   supportMail = 'Nexeed.Helpdesk@de.bosch.com';
 
