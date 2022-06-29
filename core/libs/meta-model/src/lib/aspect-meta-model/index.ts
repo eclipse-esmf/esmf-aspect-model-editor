1dabef9fe48c79247566d28652bf21628c57d879
DE\MGC1FE <mgc1fe@bosch.com>
2022-06-29 15:43:27 +0300

    init abstract property

--- core/libs/meta-model/src/lib/aspect-meta-model/index.ts
+++ core/libs/meta-model/src/lib/aspect-meta-model/index.ts
@@ -56,3 +56,4 @@ export * from './default-duration';
 export * from './default-event';
 export * from './overwritten-property';
 export * from './default-abstract-entity';
+export * from './default-abstract-property';
