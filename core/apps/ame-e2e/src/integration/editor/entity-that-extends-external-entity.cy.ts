e8a1df63135827c00bdf9ae53c630c37c97afaa6
larisaVasile <external.georgiana-larisa.vasile@bosch.com>
2024-04-03 13:31:23 +0300

    add e2e for loading a model with an entity that extends external entity

--- core/apps/ame-e2e/src/integration/editor/entity-that-extends-external-entity.cy.ts
+++ core/apps/ame-e2e/src/integration/editor/entity-that-extends-external-entity.cy.ts
@@ -13,22 +13,67 @@
 
 /// <reference types="Cypress" />
 
-import {
-  SELECTOR_notificationsBtn,
-  SELECTOR_notificationsDialogCloseButton,
-} from '../../support/constants';
-
+import {SELECTOR_notificationsBtn, SELECTOR_notificationsDialogCloseButton} from '../../support/constants';
 
 describe('Test loading aspect with extended external Entity', () => {
-  it('should display an error that external reference that is not included in the namespace file structure', () => {
+  it('should display an error that external reference is not included in the namespace file structure', () => {
     cy.intercept('POST', 'http://localhost:9091/ame/api/models/validate', {fixture: 'model-validation-response.json'});
     cy.visitDefault();
     cy.fixture('/external-reference/same-namespace/model-with-extended-entity.txt')
       .then(rdfString => cy.loadModel(rdfString))
       .then(() => {
-        cy.get(SELECTOR_notificationsBtn).click({force: true})
-        .then(() => cy.wait(500).get('.mat-mdc-cell').contains(' The Aspect model contains an external reference that is not included in the namespace file structure or is invalid').should('exist'))
-        .then(() => cy.wait(500).get(SELECTOR_notificationsDialogCloseButton).click({force: true}))
+        cy.get(SELECTOR_notificationsBtn)
+          .click({force: true})
+          .then(() =>
+            cy
+              .wait(500)
+              .get('.mat-mdc-cell')
+              .contains(
+                ' The Aspect model contains an external reference that is not included in the namespace file structure or is invalid'
+              )
+              .should('exist')
+          )
+          .then(() => cy.wait(500).get(SELECTOR_notificationsDialogCloseButton).click({force: true}));
+      });
+  });
+
+  it('should load a model with an entity that extends an external entity', () => {
+    cy.intercept('POST', 'http://localhost:9091/ame/api/models/validate', {fixture: 'model-validation-response.json'});
+    cy.intercept('GET', 'http://localhost:9091/ame/api/models/namespaces?shouldRefresh=true', {
+      'org.eclipse.examples:1.0.0': ['example.txt'],
+    });
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'http://localhost:9091/ame/api/models',
+        headers: {
+          namespace: 'org.eclipse.examples:1.0.0',
+          'file-name': 'example.txt',
+        },
+      },
+      {
+        fixture: '/external-reference/same-namespace/model-with-entity.txt',
+      }
+    );
+
+    cy.visitDefault();
+    cy.fixture('/external-reference/same-namespace/model-with-extended-entity')
+      .as('rdfString')
+      .then(rdfString => cy.loadModel(rdfString))
+      .wait(500)
+      .then(() => cy.getAspect())
+      .then(aspect => {
+        expect(aspect.name).to.equal('SimpleAspect');
+        cy.shapeExists('SimpleAspect').then(() => {
+          cy.shapeExists('SimpleEntity1');
+          cy.shapeExists('Entity1');
+        });
+      })
+      .then(() => cy.getUpdatedRDF())
+      .then(rdf => {
+        expect(rdf).to.contain('SimpleEntity1 a samm:Entity');
+        expect(rdf).to.contain('samm:extends :Entity1');
       });
   });
 });
