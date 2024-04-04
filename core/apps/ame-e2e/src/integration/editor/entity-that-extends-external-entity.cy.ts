0add7c6044f5fe2fd252fbf260a9f40631e3535a
larisaVasile <external.georgiana-larisa.vasile@bosch.com>
2024-04-04 11:14:09 +0300

    add more e2e

--- core/apps/ame-e2e/src/integration/editor/entity-that-extends-external-entity.cy.ts
+++ core/apps/ame-e2e/src/integration/editor/entity-that-extends-external-entity.cy.ts
@@ -78,6 +78,42 @@ describe('Test loading aspect with extended external Entity', () => {
   });
 
   it('should load a model with an entity that extends an external entity in different namespace', () => {
+    cy.intercept('POST', 'http://localhost:9091/ame/api/models/validate', {fixture: 'model-validation-response.json'});
+    cy.intercept('GET', 'http://localhost:9091/ame/api/models/namespaces?shouldRefresh=true', {
+      'org.eclipse.different:1.0.0': ['example.txt'],
+    });
 
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'http://localhost:9091/ame/api/models',
+        headers: {
+          namespace: 'org.eclipse.different:1.0.0',
+          'file-name': 'example.txt',
+        },
+      },
+      {
+        fixture: '/external-reference/different-namespace/model-with-entity.txt',
+      }
+    );
+
+    cy.visitDefault();
+    cy.fixture('/external-reference/different-namespace/model-with-entity-that-extends-from-different-namespace')
+      .as('rdfString')
+      .then(rdfString => cy.loadModel(rdfString))
+      .wait(500)
+      .then(() => cy.getAspect())
+      .then(aspect => {
+        expect(aspect.name).to.equal('AspectWithExtendedEntity');
+        cy.shapeExists('AspectWithExtendedEntity').then(() => {
+          cy.shapeExists('Entity1');
+          cy.shapeExists('DifferentEntity1');
+        });
+      })
+      .then(() => cy.getUpdatedRDF())
+      .then(rdf => {
+        expect(rdf).to.contain(':Entity1 a samm:Entity');
+        expect(rdf).to.contain('samm:extends ext-different:DifferentEntity1');
+      });
   });
 });
