a25e8d898a777984b7f455e9edb4da032a4f9bd6
Michele Santoro <michele.santoro@de.bosch.com>
2026-09-08 14:12:39 +0200

    fix(e2e): register default interceptors on visitDefault for before hooks

--- core/apps/ame-e2e/src/support/commands.ts
+++ core/apps/ame-e2e/src/support/commands.ts
@@ -19,7 +19,7 @@ import {SearchesStateService} from '@ame/utils';
 import {Aspect} from '@esmf/aspect-model-loader';
 import {EventObject, InternalEvent} from '@maxgraph/core';
 import 'cypress-file-upload';
-import {API_BASE_URL, FORMAT_API_URL, VALIDATE_API_URL} from './api-mocks';
+import {API_BASE_URL, FORMAT_API_URL, setUpDefaultInterceptors, VALIDATE_API_URL} from './api-mocks';
 import {FIELD_see, SELECTOR_editorSaveButton, SELECTOR_tbConnectButton} from './constants';
 import {cyHelp} from './helpers';
 
@@ -293,6 +293,7 @@ declare global {
 }
 
 Cypress.Commands.add('visitDefault', () => {
+  setUpDefaultInterceptors();
   cy.visit('/editor?e2e=true');
   cy.get('ame-loading-screen', {timeout: 15000}).should('not.exist');
   return cy.get('#graph', {timeout: 15000}).should('be.visible');
